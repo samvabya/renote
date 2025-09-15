34e6bc566a788692dd5ac388625aaaf227c0d0e7
Samvabya Sarkar <samvabyat1@gmail.com>
2025-09-15 19:55:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:renote/screens/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: lightDynamic,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
+          darkTheme: ThemeData(
+            useMaterial3: true,
+            colorScheme: darkDynamic,
+            fontFamily: GoogleFonts.poppins().fontFamily,
+          ),
+          themeMode: ThemeMode.system,
+          home: HomeScreen(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            centerTitle: true,
+            title: Container(
+              height: 56,
+              padding: const EdgeInsets.only(left: 20, right: 5),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Theme.of(context).colorScheme.surfaceVariant,
+              ),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/renote.png',
+                    width: 60,
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.view_agenda_outlined),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircleAvatar(
+                  radius: 18,
+                  child: Icon(Icons.person, size: 18),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
